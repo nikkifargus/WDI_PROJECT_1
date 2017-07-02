2186c18e18dc825a451ee490a56e2be06a3e05a2
nikkifargus <nikki@fargus.co.uk>
2017-07-02 20:23:53 +0100

    new way of creating objects to fall more randomly

--- js/app.js
+++ js/app.js
@@ -11,108 +11,89 @@
 var canvas = document.getElementById('canvas');
 canvas.width = 500;
 canvas.height = window.innerHeight;
-var context = canvas.getContext('2d');
-var img = new Image();
-img.src = 'js/star.png';
-var noOfStars = 20;
-var stars = [];
+var ctx = canvas.getContext('2d');
 
-for (var i= 0; i < noOfStars; i++){
-  var x = Math.floor(Math.random()*canvas.width);
-  var y = Math.floor(Math.random()*canvas.height);
-  stars[i] = new star(x,y);
-}
+//create a new object every 400ms
+var starCreate = 400;
 
-function star(x,y) {
-  this.x = x;
-  this.y = y;
+//how fast stars will fall
+var starFall = 1;
 
-  this.fall = function(){
-    this.y = this.y+4;
-    if(this.y> canvas.height){
-      this.y = 0;
-    }
+//when last star created
+var lastStar = -1;
+
+//an array to hold all of the created stars
+var objects = [];
+
+// var images = {img1, img2};
+var img1 = new Image();
+img1.src = 'js/star.png';
+// var img2 = new Image();
+// img2.src = 'js/planet.png';
+
+window.onload = function(){
+  animate();
+};
+
+//create new object
+function CreateRandomstar() {
+
+  //select a random type for this new object (can use later to color objects)
+  var t = 'red';
+  //
+  // if(Math.random() < 0.5){
+  //   t ='red';
+  // } else {
+  //   t='blue';
+  // }
+
+  var object = {
+    //set this object type
+    type: t,
+    //set x randomly but at least 10px off the canvas edges
+    x: Math.random() * (canvas.width -20) + 10,
+    //set y to start at top of page
+    y: 0
   };
 
-  this.show = function(){
-    context.drawImage(img,this.x, this.y, 25,25);
-
-  };
+  // add the new object to the objects[] array
+  objects.push(object);
 }
 
-function draw() {
-  context.fillStyle = 'black';
-  context.fillRect(0,0, canvas.width, canvas.height);
-  for (var i = 0; i < noOfStars; i++) {
-    stars[i].show();
-    stars[i].fall();
+function animate(){
+  //get the elapsed time
+  var time = Date.now();
+
+  //see if its time to create a new star
+  if (time> (lastStar + starCreate)){
+    lastStar = time;
+    CreateRandomstar();
+  }
+
+  //request another animation frame
+  requestAnimationFrame(animate);
+
+  //clear the canvas so all objects can be redrawn
+  ctx.clearRect(0,0, canvas.width, canvas.height);
+
+  //draw the line where the new stars are created
+  ctx.beginPath();
+  ctx.moveTo(0, 0);
+  ctx.stroke();
+
+  image: images[Math.floor(Math.random()*images.length)];
+
+  //add style and move each object down the canvas
+  for (var i = 0; i < objects.length; i++) {
+    var object = objects[i];
+    object.y += starFall;
+    ctx.drawImage(object.image, object.x, object.y, 30, 30);
+    // context.drawImage(img,object.x, object.y, 25,25);
+    // context.fillRect(0,0, canvas.width, canvas.height);
+    // ctx.beginPath();
+    // ctx.arc(object.x, object.y, 8, 0, Math.PI*2);
+    // ctx.closePath();
+    // ctx.fillStyle = object.type;
+    // ctx.fill();
   }
 }
-
-function update(){
-  draw();
-  window.requestAnimationFrame(update);
-  $(document).mousemove(function(e) {
-    $('#cursor').offset({
-      left: e.pageX,
-      top: e.pageY + 20
-    });
-  });
-}
-
-update();
-
-//
-// function setUp(){
-//   $('main').mouseenter(function(){
-//     console.log('in main');
-//     // $(document).mousemove(function(e){
-//     //   $('#rocket').css({left: e.pageX, top: e.pageY});
-//     // });
-//   });
-// }
-//
-// setUp();
-
-//
-// $(document).ready(function(){
-//   var $moveable = $('.moveAble');
-//   $(document).mousemove(function(e){
-//     $moveable.css({'top': e.pageY,'left': e.pageX});
-//   });
-// });
-
-// function myMove() {
-//   var elem = document.getElementById("myAnimation");
-//   var pos = 0;
-//   var id = setInterval(frame, 10);
-//   function frame() {
-//     if (pos == 350) {
-//       clearInterval(id);
-//     } else {
-//       pos++;
-//       elem.style.top = pos + 'px';
-//       // elem.style.left = pos + 'px';
-//     }
-//   }
-// }
-//
-// function myMove(){
-//   var div = document.createElement('div');
-//   div.style.width = '100px';
-//   div.style.height = '100px';
-//   div.style.background = 'white';
-//   div.style.color = 'white';
-//   div.innerHTML = 'Hello';
-// }
-//
-// function setup (){
-//   $('button').on('click', (myMove));
-// }
-//
-// function init(){
-//   setup();
-// }
-//
-//
-// $(init);
