4525ad03c286786921d96b271d42772a9337eb7f
nikkifargus <nikki@fargus.co.uk>
2017-07-03 14:02:53 +0100

    added some styling to stars

--- js/app.js
+++ js/app.js
@@ -7,12 +7,6 @@
 // create function so stars fall randomly
 // if star hits rocket, rocket explodes
 
-
-
-
-//hide original cursor when enter canvas
-// canvas.style.cursor = 'none';
-
 //create a new object every 300ms
 var starCreate = 100;
 
@@ -24,6 +18,8 @@ var lastStar = -1;
 
 //an array to hold all of the created stars
 var objects = [];
+
+var rocket = '.rocket';
 
 
 
@@ -49,42 +45,63 @@ function CreateRandomstar() {
 
 function animate(){
 
-  // $('main').mouseenter(function(){
-  //   ctx.fillStyle ='green';
-  //   ctx.fillRect(225, 500, 100, 100);
-  // });
-  //get the elapsed time
-  var time = Date.now();
+  animateStar();
+  moveRocket();
+  detectCollision();
 
-  //see if its time to create a new star
-  if (time> (lastStar + starCreate)){
-    lastStar = time;
-    CreateRandomstar();
+  function moveRocket(){
+    $('main').mouseenter(function(){
+      ctx.fillStyle = 'gray';
+      ctx.fillRect(20, 20, 50, 50);
+      $(document).mousemove(function(e){
+        $(rocket).css({left: e.pageX, top: e.pageY});
+      });
+    });
   }
 
-  //request another animation frame  (used instead of set interval)
-  requestAnimationFrame(animate);
+  function animateStar (){
+    // used to get the elapsed time
+    var time = Date.now();
 
-  //clear the canvas so all objects can be redrawn
-  ctx.clearRect(0,0, canvas.width, canvas.height);
+    //see if its time to create a new star
+    if (time> (lastStar + starCreate)){
+      lastStar = time;
+      CreateRandomstar();
+    }
 
-  //add size & color and move each object down the canvas
-  for (var i = 0; i < objects.length; i++) {
-    var object = objects[i];
-    object.y += starFall;
-    ctx.fillStyle = 'red';
-    ctx.fillRect(object.x, object.y, 10, 10);
+    //request another animation frame  (used instead of set interval)
+    requestAnimationFrame(animate);
 
+    //clear the canvas so all objects can be redrawn
+    ctx.clearRect(0,0, canvas.width, canvas.height);
+
+    //add size & color and move each object down the canvas
+    for (var i = 0; i < objects.length; i++) {
+      var object = objects[i];
+      object.y += starFall;
+      ctx.beginPath();
+      ctx.arc(object.x, object.y, 7, 0,2*Math.PI);
+      var grd=ctx.createRadialGradient(75,50,5,90,60,500);
+      grd.addColorStop(0,'gold');
+      grd.addColorStop(1,'white');
+      ctx.fillStyle = grd;
+      ctx.fill();
+      ctx.stroke();
+    }
+  }
+
+  function detectCollision(objects){
+    for (var i = 0; i < objects.length; i++) {
+      var o = objects[i];
+      if(rocket.x + rocket.width >= o.x && rocket.x <= o.x + o.width && rocket.y >= o.y && rocket.y <= o.y + o.height ){
+        console.log('hit');
+      }
+    }
   }
 }
 
 window.onload = function(){
   animate();
-  $('main').mouseenter(function(){
-    $(document).mousemove(function(e){
-      $('.rocket').css({left: e.pageX, top: e.pageY});
-    });
-  });
 };
 
 
@@ -98,3 +115,6 @@ ctx.fillStyle ='green';
 // $(document).mousemove(function(e){
 //   $('#cursor').css({left: e.pageX, top: e.pageY});
 // });
+
+//hide original cursor when enter canvas
+// canvas.style.cursor = 'none';
